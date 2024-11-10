4454f4cae2e8a885ac9c9d9b293e65c334970367
Shrikrishna <sbbordeshrikrishna@gmail.com>
2024-11-10 17:48:37 +0530

    dynamic class

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -8,7 +8,8 @@
         greet: "Hello",
         channel: "<b>codeevoluation</b>",
         headingId:"heading",
-        isDisabled: true
+        isDisabled: true,
+        className: 'danger'
       }
     }
   }
@@ -19,5 +20,15 @@
   <div v-html="channel"></div>
   <h2 v-bind:id="headingId">Heading</h2>
   <button v-bind:disabled="isDisabled">Button</button>
+  <h2 class="underline" v-bind:class="className">Underlined text</h2>
 </template>
+
+<style>
+
+.underline {
+  text-decoration: underline;
+}
+
+</style>
+
 
