7c93780194732713861d8c3accae12ac757d5f78
Shrikrishna <sbbordeshrikrishna@gmail.com>
2024-11-10 18:31:12 +0530

    v-bind usage shortcut

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -24,14 +24,14 @@
 <template>
   <div>{{greet}} {{ name }}</div>
   <div v-html="channel"></div>
-  <h2 v-bind:id="headingId">Heading</h2>
-  <button v-bind:disabled="isDisabled">Button</button>
-  <h2 class="underline" v-bind:class="className">Underlined text</h2>
-  <h2 v-bind:class="isPromoted && 'promoted'">Promoted movie</h2>
-  <h2 v-bind:class="isSoldOut ? 'soldOut': 'new'">Sold Out ?</h2>
-  <h2 v-bind:class="['new','promoted']">Newly Promoted Movie</h2>
-  <h2 v-bind:class="[isPromoted && 'promoted', isSoldOut ? 'soldOut': 'new']">Array conditional movie</h2>
-  <h2 v-bind:class="{
+  <h2 :id="headingId">Heading</h2>
+  <button :disabled="isDisabled">Button</button>
+  <h2 class="underline" :class="className">Underlined text</h2>
+  <h2 :class="isPromoted && 'promoted'">Promoted movie</h2>
+  <h2 :class="isSoldOut ? 'soldOut': 'new'">Sold Out ?</h2>
+  <h2 :class="['new','promoted']">Newly Promoted Movie</h2>
+  <h2 :class="[isPromoted && 'promoted', isSoldOut ? 'soldOut': 'new']">Array conditional movie</h2>
+  <h2 :class="{
     promoted: isPromoted,
     new: !isPromoted,
     soldOut: isSoldOut
