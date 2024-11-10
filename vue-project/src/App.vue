34c4edac6ee8e079c9572bd4698f886f5ec859e7
Shrikrishna <sbbordeshrikrishna@gmail.com>
2024-11-10 17:35:30 +0530

    v-bind

--- vue-project/src/App.vue
+++ vue-project/src/App.vue
@@ -6,7 +6,9 @@
       return{
         name: "Batman",
         greet: "Hello",
-        channel: "<b>codeevoluation</b>"
+        channel: "<b>codeevoluation</b>",
+        headingId:"heading",
+        isDisabled: true
       }
     }
   }
@@ -15,9 +17,7 @@
 <template>
   <div>{{greet}} {{ name }}</div>
   <div v-html="channel"></div>
+  <h2 v-bind:id="headingId">Heading</h2>
+  <button v-bind:disabled="isDisabled">Button</button>
 </template>
 
-<style scoped>
-
-
-</style>
